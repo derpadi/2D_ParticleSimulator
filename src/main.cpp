933eb6f4e3e3aff910ed4598238f72d45a7a6a6b
derpadi <pagoehringer@gmail.com>
2026-07-04 20:15:42 +0200

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 #include "rlImGui.h"
 
 
-
-
 int main() {
 {
     const int screenWidth = 800;
@@ -15,10 +13,11 @@ int main() {
     rlImGuiSetup(true);
 
 
-    float v_x = 20.0f;
-    float v_y = 0.0f;
-    float pos_x = 300.0f;
-    float pos_y = 200.0f;
+    float radius = 10.0f;
+
+    Vector2 position = { 300.0f, 200.0f };
+    Vector2 velocity = { 20.0f, 0.0f};
+    float bounciness = 1.0f;
 
     float scale = 10.0f;
     //float timescale = 0.1f;
@@ -31,34 +30,31 @@ int main() {
         // -------------------------------------- CODE GOES HERE --------------------------------------
 
         float dt = GetFrameTime();
-        v_y += 9.81f * dt;
-        pos_y += v_y * dt * scale;
 
-        //v_x *= 0.9f;
-        pos_x += v_x * dt * scale;
+        // Updating physics
+        velocity.y += 9.81f * dt;
 
-        if(pos_y+10 >= screenHeight){
-            v_y *= -1.0f;
-            pos_y = screenHeight-10.0f;
+        position.x += velocity.x * dt * scale;
+        position.y += velocity.y * dt * scale;
+
+        // Checking for wall collisions
+        if(position.y+radius >= screenHeight){
+            velocity.y *= -bounciness;
+            position.y = screenHeight-radius;
+        } else if(position.y-radius <= 0){
+            velocity.y *= -bounciness;
+            position.y = 0 + radius;
         }
 
-        if(pos_y-10 <= 0){
-            v_y *= -1.0f;
-            pos_y = 0 + 10.0f;
+        if(position.x-radius <= 0){
+            velocity.x *= -bounciness;
+            position.x = 0 + radius;
+        } else if(position.x+radius >= screenWidth){
+            velocity.x *= -bounciness;
+            position.x = screenWidth - radius;
         }
 
-        if(pos_x-10 <= 0){
-            v_x *= -1.0f;
-            pos_x = 0 + 10.0f;
-        }
-
-        if(pos_x+10 >= screenWidth){
-            v_x *= -1.0f;
-            pos_x = screenWidth - 10.0f;
-        }
-
-        Vector2 test = { pos_x, pos_y };
-        DrawCircleLinesV(test, 10, RED);
+        DrawCircleLinesV(position, radius, RED);
 
 
 
@@ -67,9 +63,8 @@ int main() {
         rlImGuiBegin();
         ImGui::Begin("Hello, world!");
         ImGui::Text("This in my control panel!");
-        ImGui::Text("Velocity (x,y): (%.2f, %.2f) m/s", v_x, v_y);
-
-        ImGui::Text("Position (x,y): (%.2f,%.2f) m", pos_x, pos_y);
+        ImGui::Text("Velocity (x,y): (%.2f, %.2f) m/s", velocity.x, velocity.y);
+        ImGui::Text("Position (x,y): (%.2f,%.2f) m", position.x, position.y);
         ImGui::End();
         rlImGuiEnd();
 
